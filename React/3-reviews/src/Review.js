5133c98da5949c5d7fd6ef9b98d6fd4021c3be44
Jugo-JS <jugoslavsubotic@gmail.com>
2021-03-30 15:00:24 +0200

    adding start code without funcionalities

--- React/3-reviews/src/Review.js
+++ React/3-reviews/src/Review.js
@@ -3,7 +3,34 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  return (
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <h2>our reviews</h2>
+          <div className='underline'></div>
+        </div>
+          <article className='review'>
+            <div className='img-container'>
+              <img src='' alt='image' />
+              <span class='quote-icon'>
+                <FaQuoteRight />
+              </span>
+            </div>
+            <h4 className='author'>susan</h4>
+            <p className='job'>web developer</p>
+            <p className='info'>
+              I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry
+            </p>
+            <div className='button-container'>
+              <button className='prev-btn'><FaChevronLeft /></button>
+              <button className='next-btn'><FaChevronRight /></button>
+            </div>
+            <button className='random-btn'>Suprise me</button>
+          </article>
+      </section>
+    </main>
+  );
 };
 
 export default Review;
