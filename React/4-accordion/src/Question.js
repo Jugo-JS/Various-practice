e5502054ffe046ceca850a300eb3404197256b45
Jugo-JS <jugoslavsubotic@gmail.com>
2021-04-01 13:10:28 +0200

    finishing accordion React project

--- React/4-accordion/src/Question.js
+++ React/4-accordion/src/Question.js
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+const Question = ({ title, info }) => {
+  const [isInfoVisible, setIsInfoVisible] = useState(false);
+  
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        {isInfoVisible ? 
+          <button className='btn' onClick={() => setIsInfoVisible(false)}><AiOutlineMinus /></button> 
+          : 
+          <button className='btn' onClick={() => setIsInfoVisible(true)}><AiOutlinePlus /></button>}
+      </header>
+      {isInfoVisible && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
