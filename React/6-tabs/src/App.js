66e4c8492edf71324b06e2c7bbff1a1f218313c3
Jugo-JS <jugoslavsubotic@gmail.com>
2021-04-02 15:13:11 +0200

    finishing React tabs project

--- React/6-tabs/src/App.js
+++ React/6-tabs/src/App.js
@@ -1,10 +1,68 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+import React, { useState, useEffect } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
+
+const url = 'https://course-api.com/react-tabs-project';
+
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [companies, setCompanies] = useState([]);
+  const [value, setValue] = useState(0);
+ 
+ 
+  const fetchData = async() => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setCompanies(data);
+    setLoading(false);
+  }
+  
+  useEffect(() => {
+    fetchData();
+    }, [])
+
+  if(loading) {
+    return <div className='section loading'>Loading...</div>
+  }
+
+  const { company, duties, dates, title } = companies[value];
+
+  return (
+      <section className='section'>
+      <div>
+        <div className='title'>
+          <h2>expierence</h2>
+          <div className='underline'></div>
+        </div>
+        <div className='jobs-center'>
+          <div className='btn-container'>
+            {companies.map((company, index) => {
+            return (
+              <button 
+                key={company.id} 
+                className={`job-btn ${index === value && 'active-btn'}`} 
+                onClick={() => setValue(index)}
+              >
+                {company.company}
+              </button>)
+            })}
+          </div>
+              <article  className='job-info'>
+                <h3>{title}</h3>
+                <h4>{company}</h4>
+                <p className='job-date'>{dates}</p>
+                {duties.map((duty, index) => {
+                  return (
+                    <div key={index} className='job-desc'>
+                      <FaAngleDoubleRight className='job-icon' />
+                      <p>{duty}</p>
+                    </div>
+                  )
+                })}
+              </article>
+        </div>
+      </div>
+    </section>
+  )
 }
 
-export default App
+export default App;
