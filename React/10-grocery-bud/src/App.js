3bfeb623398e0bd21d0010ccd65a5ef4898f9908
Jugo-JS <jugoslavsubotic@gmail.com>
2021-04-07 13:51:28 +0200

    adding first funcionalities to the project

--- React/10-grocery-bud/src/App.js
+++ React/10-grocery-bud/src/App.js
@@ -1,9 +1,49 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from 'react';
+import List from './List';
+import Alert from './Alert';
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [listItem, setListItem] = useState('');
+  const [list, setList] = useState([]);
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    if(listItem !== false) {
+      let newItem = {
+        id: new Date().getTime().toString(),
+        title: listItem
+      };
+      setList([...list, newItem])
+      setShowAlert(true);
+      setListItem('');
+    }
+  }
+
+  return (
+    <section className='section-center'>
+      {showAlert && <Alert />}
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        <h3>grocery bud</h3>
+        <div className='form-control'>
+          <input 
+            type='text' 
+            className='grocery' 
+            placeholder='e.g. eggs' 
+            value={listItem}
+            onChange={(event) => setListItem(event.target.value)}
+          />
+          <button type='submit' className='submit-btn'>submit</button>
+        </div>
+      </form>
+      <div className='grocery-container'>
+        
+          <List items={list} />
+        </div>
+        <button className='clear-btn'>clear items</button>
+    </section>
+  )
 }
 
-export default App
+export default App;
