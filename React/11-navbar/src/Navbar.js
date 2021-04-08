7c6b2e8d7a6bd20a89fe995c46c720a3799fc29b
Jugo-JS <jugoslavsubotic@gmail.com>
2021-04-08 14:46:14 +0200

    finishing React navbar project

--- React/11-navbar/src/Navbar.js
+++ React/11-navbar/src/Navbar.js
@@ -1,10 +1,49 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { links, social } from './data';
+import logo from './logo.svg';
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  const [linkList, setLinkList] = useState(links);
+  const [socialIconsList, setSocialIconsLIst] = useState(social);
+  const [showLinks, setShowLinks] = useState(false);
+  const linksRef = useRef(null)
+  const linkContainer = useRef(null);
+  
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;   
+    if(showLinks) {
+      linkContainer.current.style.height = `${linksHeight}px`;
+    } else {
+      linkContainer.current.style.height = '0px';
+    }
+  }, [showLinks])
+  
+
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='logo' />
+          <button className='nav-toggle' onClick={() => setShowLinks(!showLinks)}><FaBars /></button>
+        </div>
+        <div className='links-container' ref={linkContainer}>
+          <ul className='links' ref={linksRef}>
+            {linkList.map((link) => {
+              const { id, url, text } = link;
+              return <li key={id}><a href={url}>{text}</a></li>
+            })}
+          </ul>
+        </div>
+        <ul className='social-icons'>
+          {socialIconsList.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return <li key={id}><a href={url}>{icon}</a></li>
+          })}
+        </ul>
+      </div>
+    </nav>
+  )
 }
 
-export default Navbar
+export default Navbar;
