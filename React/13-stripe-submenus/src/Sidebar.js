0f717b30aba13db3d17c63d632aae80f789ba8e3
Jugo-JS <jugoslavsubotic@gmail.com>
2021-04-12 14:09:01 +0200

    adding html, css and toggle sidebar

--- React/13-stripe-submenus/src/Sidebar.js
+++ React/13-stripe-submenus/src/Sidebar.js
@@ -1,9 +1,37 @@
-import React from 'react'
-import { FaTimes } from 'react-icons/fa'
-import sublinks from './data'
+import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+import sublinks from './data';
+import { useGlobalContext } from './context';
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
+  const Context = useGlobalContext();
+
+  return (
+      <aside className={`${Context.showSubmenu ? 'sidebar-wrapper show' : 'sidebar-wrapper'}`}>
+      <div className='sidebar'>
+        <button className='close-btn' onClick={Context.toggleSubmenu}><FaTimes /></button>
+        <div className='sidebar-links'>
+          {sublinks.map((sublink, index) => {
+            const { page, links } = sublink;
+            return (
+              <article key={index}>
+                <h4>{page}</h4>
+                <div className='sidebar-sublinks'>
+                  {links.map((link, index) => {
+                    const { label, icon, url } = link;
+                    return (
+                      <a href={url} key={index}>{icon} {label}</a>
+                    )
+                  })}
+                  
+                </div>
+              </article>
+            )
+          })}
+        </div>
+      </div>
+    </aside>
+  )
 }
 
-export default Sidebar
+export default Sidebar;
--- React/13-stripe-submenus/src/context.js
+++ React/13-stripe-submenus/src/context.js
@@ -1,2 +1,30 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+    const [showSubmenu, setShowSubmenu] = useState(false);
+
+    const toggleSubmenu = () => {
+        setShowSubmenu(showSubmenu => !showSubmenu);
+    }
+
+
+    return (
+        <AppContext.Provider 
+            value={{
+                showSubmenu,
+                toggleSubmenu
+            }}
+        >
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+}
+
+export { AppProvider }
