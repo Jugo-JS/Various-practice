bdec5d24b349c9e2a9e87e3055a4283723173221
Jugo-JS <jugoslavsubotic@gmail.com>
2021-05-04 15:01:22 +0200

    adding first components and starting styles

--- React/21-movie-db/src/Movies.js
+++ React/21-movie-db/src/Movies.js
@@ -1,11 +1,24 @@
-import React from 'react'
-import { useGlobalContext } from './context'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { useGlobalContext } from './context';
+import { Link } from 'react-router-dom';
+
 const url =
-  'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
+  'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png';
 
 const Movies = () => {
-  return <h2>movies component</h2>
+  return (
+    <section className='movies'>
+      <a className='movie' href='#'>
+        <article>
+          <img src={url} alt='' />
+          <div className='movie-info'>
+            <h4 className='title'>Batman Begins</h4>
+            <p>2005</p>
+          </div>
+        </article>
+      </a>
+    </section>
+  )
 }
 
-export default Movies
+export default Movies;
