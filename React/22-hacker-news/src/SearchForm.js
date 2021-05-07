40a1b82e9eb5e1ada81d157836e4222fe7f51937
Jugo-JS <jugoslavsubotic@gmail.com>
2021-05-07 13:54:09 +0200

    adding first components and styles

--- React/22-hacker-news/src/SearchForm.js
+++ React/22-hacker-news/src/SearchForm.js
@@ -2,7 +2,12 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const SearchForm = () => {
-  return <h2>search form</h2>
+  return (
+    <form className='search-form'>
+      <h2>Search Hacker News</h2>
+      <input type='text' className='form-input' value='react' />
+    </form>
+  )
 }
 
 export default SearchForm
--- React/22-hacker-news/src/Stories.js
+++ React/22-hacker-news/src/Stories.js
@@ -3,7 +3,18 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const Stories = () => {
-  return <h2>stories component</h2>
+  return (
+    <section className='stories'>
+      <article className='story'>
+        <h4 className='title'>Relicensing React, Jest, Flow, and Immutable.js</h4>
+        <p className='info'>2280  points by <span>pomber | </span>108 comments</p>
+        <div>
+          <a href='#' className='read-link' target='_blank'>Read More</a>
+          <button className='remove-btn'>remove</button>
+        </div>
+      </article>
+    </section>
+  )
 }
 
 export default Stories
